16fcbe1bf6d47df9e908641f9f75b316eff27f72
velobosv1977 <velobosv33@gmail.com>
2024-11-20 15:53:49 +0300

    Initial Commit Massive

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.println("Massive. Lesson 1");
+        int[] box = {3, 6, 7,};
+        System.out.println(box[0]);
+
+        System.out.println("");
+        // Создаём массив на 12 ячеек
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        // Запуск цикла по выводу массива
+        for (int i = 0; i < 12; i++) {
+            System.out.println(weights[i]);
         }
+        System.out.println("");
+        int[] houses = new int[7];
+        houses[0] = 5;
+        houses[1] = 7;
+        houses[6] = 10;
+        System.out.println(houses[0]);
+        System.out.println(houses[1]);
+        System.out.println(houses[6]);
     }
 }
